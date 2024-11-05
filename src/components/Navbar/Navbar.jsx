bed598bebbb53969f949b76ee91c2a372d8653f5
Arowolo Sodiq <sodiqarowolo02@gmail.com>
2024-11-05 12:54:47 +0100

    adding menu icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import logo from "../../assets/images/logo.png"
 import {navLinksdata } from "../../constants/index"
 import {Link} from "react-scroll"
+import { IoMenu } from "react-icons/io5";
 
 const Navbar = () => {
   return (
     <div className='w-full h-24 mx-auto sticky top-0 z-50 bg-bodyColor flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600'>
         <div>
-            <img className='w-16' src={logo} alt="" />
+            <img className='w-32' src={logo} alt="" />
         </div>
         <div>
-             <ul className='flex items-center gap-10'>
+             <ul className='hidden lg:flex items-center gap-10'>
                 {navLinksdata.map(({_id,title,link})=>(
                     <li className='text-base font-normal text-gray-400 tracking-wide cursor-pointer
                     hover:text-designColor duration-300' key={_id}>
@@ -28,6 +29,9 @@ const Navbar = () => {
                 ))}
 
              </ul>
+             <span className='text-xl mdl:hidden bg-black w-10 h-10 inline-flex
+             items-center justify-center  rounded-full text-designColor cursor-pointer
+             '><IoMenu /></span>
         </div>
 
     </div>
